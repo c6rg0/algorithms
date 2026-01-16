6acd1971a4136ae56fb8351af20af478dcc6d953
c6rg0 <gdrozbik08@gmail.com>
2026-01-16 23:13:25 +0000

    Binary tree traversal is a WIP, and I have to add an extra condition and results output for the new bubble sort

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::mem;
+
 fn main() {
     binary_search();
     linear_search();
+    // binary_tree_sort();
     bubble_sort();
 }
 
@@ -59,8 +62,61 @@ fn linear_search(){
     }
 }
 
-fn bubble_sort(){
+fn binary_tree_sort(){
+    let array: [i32; 14] = [9, 12, 14, 27, 28, 35, 41, 50, 52, 29, 60, 66, 68, 71]; 
+    let target: i32 = 71;
+    let last: usize = (array.len() / array[0].
+        to_string().len()) - 1;
+    
+    let mut found: bool = false;
+    let current_node: i32 = 50;
+    let current_node_pos: i32 = 8;
 
+    while found == false {
+        if target == current_node {
+            found = true;
+        }
+        else if target < current_node{
+            /*
+             * if left child exists
+            if array[].contains(){
+
+            }
+            */
+        }
+    }
 }
+
+fn bubble_sort() {
+    let array: [i32; 7] = [9, 2, 7, 1934, 13, 200, 1];
+    let last: usize = (array.len() / array[0].
+        to_string().len()) - 1;
+    let index: i32 = 0;
+    let j: i32 = 0;
+    let mut swapped: bool = false;
+    println!();
+    println!("Bubble sort:");
+
+    while swapped == false {
+        let len: i32 = (last as i32)-2;
+        for index in 0..len {
+            for j in 0..len-index-2 {
+                if array[j as usize] > array[(j + 1) as usize] {
+                    let mut x = array[(index + 1) as usize];
+                    let mut y = array[index as usize];
+                    println!("OG X: {x}");
+                    println!("OG Y: {y}");
+                    mem::swap(&mut x, &mut y);
+                    println!("Swapped X: {x}");
+                    println!("Swapped Y: {y}");
+                    swapped = true;
+                }
+            }
+        }
+    }
+}
+
+
+
 
 
